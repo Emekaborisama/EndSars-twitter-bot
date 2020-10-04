1121360d59ebf56219202d6a7931d3ae3cd05f66
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-04 19:36:36 +0100

    Update comment_bot.py

--- comment_bot.py
+++ comment_bot.py
@@ -4,22 +4,28 @@ import os
 from time import sleep
 
 
-import config
+auth = tp.OAuthHandler('HIKNUXgmi73ArgiofX7TsHWXG', 'jqCot90uTJbeaVivJQdKB7i6sOVc6nnU8gUrB6llMixOexUtAH')
+auth.set_access_token('1305611268447928320-mUXqZUiDrTHxVB1gd5JowLvNjF0JkB', 'gOj1wF0IEKetKrA61UoSoWWU2BOLk6ppdknwQsmoQGK3p')
+api = tp.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+    
 
-usersname = '@emeka_boris'
 
-def comment__():
+
+def comment(usersname):
     time_line = api.user_timeline(usersname, count = 1)
     user = api.get_user(usersname)
     for tweet in time_line:
-        name = tweet.user.screen_name
         id = tweet.id
         print(f"{tweet.user.name} said {tweet.text} with {id}" )
         try:
-            api.update_status("#Endsarznow", in_reply_to_status_id = tweet.id, auto_populate_reply_metadata=True)
+            api.update_status("#EndSars now", in_reply_to_status_id = tweet.id, auto_populate_reply_metadata=True)
             api.update_status(usersname + '    ' + "Just tweeted")
-            sleep(5)
+        #success = True
         except:
             pass
-    
     
