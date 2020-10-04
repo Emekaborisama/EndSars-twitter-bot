3d5d0409fe8a0928df932c2ad4a27ecbbb222bee
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-04 15:03:24 +0100

    Add files via upload

---
+++ comment_bot.py
@@ -0,0 +1,22 @@
+import tweepy as tp
+import logging
+import os
+from time import sleep
+
+
+import config
+
+usersname = '@Mbuhari'
+
+def comment_():
+    time_line = api.user_timeline(usersname, count = 1)
+    user = api.get_user(username)
+    for tweet in time_line:
+        name = tweet.user.screen_name
+        id = tweet.id
+        print(f"{tweet.user.name} said {tweet.text} with {id}" )
+        api.update_status("#Endsarznow", in_reply_to_status_id = tweet.id, auto_populate_reply_metadata=True)
+        api.update_status(name "Just tweeted")
+        sleep(5)
+    
+    
---
+++ endsarsbot.py
@@ -0,0 +1,19 @@
+import tweepy as tp
+import logging
+import os
+from time import sleep
+
+logger = logging.getLogger()
+
+
+auth = tp.OAuthHandler('3rFqD3xxtIPoUvdEd0PNA2fly', 's2FNdygjZRCrlECZBlmk3q3KjSgJMTjvUi1dD4SXcCdt4FA6Iw')
+auth.set_access_token('1305611268447928320-ioLLdFnDhCUyvHqSyw0nT5G6XXPvb4', 'q7ZYOyJgYiSth5ZY8IohGkWSSOgGJ9R9blj2dx3HAh0DZ')
+api = tp.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
+    
+
+
