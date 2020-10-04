dcedd08c218aa7234a9a08bc597bf70eff807337
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-04 19:36:58 +0100

    Update endsarsbot.py

--- endsarsbot.py
+++ endsarsbot.py
@@ -6,8 +6,8 @@ from time import sleep
 logger = logging.getLogger()
 
 
-auth = tp.OAuthHandler('3rFqD3xxtIPoUvdEd0PNA2fly', 's2FNdygjZRCrlECZBlmk3q3KjSgJMTjvUi1dD4SXcCdt4FA6Iw')
-auth.set_access_token('1305611268447928320-ioLLdFnDhCUyvHqSyw0nT5G6XXPvb4', 'q7ZYOyJgYiSth5ZY8IohGkWSSOgGJ9R9blj2dx3HAh0DZ')
+auth = tp.OAuthHandler('HIKNUXgmi73ArgiofX7TsHWXG', 'jqCot90uTJbeaVivJQdKB7i6sOVc6nnU8gUrB6llMixOexUtAH')
+auth.set_access_token('1305611268447928320-mUXqZUiDrTHxVB1gd5JowLvNjF0JkB', 'gOj1wF0IEKetKrA61UoSoWWU2BOLk6ppdknwQsmoQGK3p')
 api = tp.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
 try:
     api.verify_credentials()
@@ -17,11 +17,20 @@ except:
     
 
 
-import comment as c
+import comment_bot as c
 import like_retweet as lrt
-import mention sd m
+import mention as m
 
-if __name__ == '__main__':
-    c.comment_()
+def main():
+    usersname = '@Mbuhari'
+    c.comment(usersname = usersname)
     lrt.like_rt()
     m.tweet_mention()
+    success = True
+    return success
+    
+    
+    
+while True:
+    main()
+    sleep(10)
