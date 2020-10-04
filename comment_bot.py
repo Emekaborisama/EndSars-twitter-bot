b036110fc000034b60ab8c0a541929a1f72548ec
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-04 15:18:27 +0100

    Update comment_bot.py

--- comment_bot.py
+++ comment_bot.py
@@ -6,17 +6,20 @@ from time import sleep
 
 import config
 
-usersname = '@Mbuhari'
+usersname = '@emeka_boris'
 
-def comment_():
+def comment__():
     time_line = api.user_timeline(usersname, count = 1)
-    user = api.get_user(username)
+    user = api.get_user(usersname)
     for tweet in time_line:
         name = tweet.user.screen_name
         id = tweet.id
         print(f"{tweet.user.name} said {tweet.text} with {id}" )
-        api.update_status("#Endsarznow", in_reply_to_status_id = tweet.id, auto_populate_reply_metadata=True)
-        api.update_status(name "Just tweeted")
-        sleep(5)
+        try:
+            api.update_status("#Endsarznow", in_reply_to_status_id = tweet.id, auto_populate_reply_metadata=True)
+            api.update_status(usersname + '    ' + "Just tweeted")
+            sleep(5)
+        except:
+            pass
     
     
