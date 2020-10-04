824bfe6172ca48854da40a58e29fad9023422305
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-04 22:31:58 +0100

    Update mention.py

--- mention.py
+++ mention.py
@@ -20,9 +20,11 @@ except:
 def tweet_mention():
     tweets= api.mentions_timeline(tweet_mode = 'extended')
     try:
-        for tweet in reversed(tweets):
-            api.update_status("@" + tweet.user.screen_name  + ' #EndSars Now!', tweet.id)
+        for tweet in tweets:
+            api.update_status('#EndSars Now!', in_reply_to_status_id = tweet.id, auto_populate_reply_metadata = True)
             print('replied the mentioned')
+        #success = True
     except:
         print('We pass')
         pass
+    #return success
