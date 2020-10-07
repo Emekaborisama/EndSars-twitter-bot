8e86650f3d3b3cd9e417929997d0d81ee8f9ab2d
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-08 00:37:55 +0100

    Update mention.py

--- mention.py
+++ mention.py
@@ -6,8 +6,8 @@ from time import sleep
 logger = logging.getLogger()
 
 
-auth = tp.OAuthHandler('3rFqD3xxtIPoUvdEd0PNA2fly', 's2FNdygjZRCrlECZBlmk3q3KjSgJMTjvUi1dD4SXcCdt4FA6Iw')
-auth.set_access_token('1305611268447928320-ioLLdFnDhCUyvHqSyw0nT5G6XXPvb4', 'q7ZYOyJgYiSth5ZY8IohGkWSSOgGJ9R9blj2dx3HAh0DZ')
+auth = tp.OAuthHandler('HIKNUXgmi73ArgiofX7TsHWXG', 'jqCot90uTJbeaVivJQdKB7i6sOVc6nnU8gUrB6llMixOexUtAH')
+auth.set_access_token('1305611268447928320-mUXqZUiDrTHxVB1gd5JowLvNjF0JkB', 'gOj1wF0IEKetKrA61UoSoWWU2BOLk6ppdknwQsmoQGK3p')
 api = tp.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
 try:
     api.verify_credentials()
@@ -21,7 +21,7 @@ def tweet_mention():
     tweets= api.mentions_timeline(tweet_mode = 'extended')
     try:
         for tweet in tweets:
-            api.update_status('#EndSarsNow', in_reply_to_status_id = tweet.id, auto_populate_reply_metadata = True)
+            api.update_status('#EndSARS #EndSARS #EndSARS #EndSARS #EndSARS #EndSARS #EndSARS #EndSARS #EndSARS', in_reply_to_status_id = tweet.id, auto_populate_reply_metadata = True)
             print('replied the mentioned')
         #success = True
     except:
