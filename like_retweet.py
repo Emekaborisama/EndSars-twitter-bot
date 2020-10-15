83a0c6381e177c177c422f7884366c7e0701b652
Emeka boris ama <borisphilosophy@gmail.com>
2020-10-15 11:26:40 +0100

    Update like_retweet.py

--- like_retweet.py
+++ like_retweet.py
@@ -6,8 +6,8 @@ from time import sleep
 logger = logging.getLogger()
 
 
-auth = tp.OAuthHandler('HIKNUXgmi73ArgiofX7TsHWXG', 'jqCot90uTJbeaVivJQdKB7i6sOVc6nnU8gUrB6llMixOexUtAH')
-auth.set_access_token('1305611268447928320-mUXqZUiDrTHxVB1gd5JowLvNjF0JkB', 'gOj1wF0IEKetKrA61UoSoWWU2BOLk6ppdknwQsmoQGK3p')
+auth = tp.OAuthHandler('pIZw54XtvVuGZM4TTJYHZrFX1', '6x1ohdPHRdFnSrUGUo6qbEup3O7eiVk5mMNpnFXcWrLQPNVhTC')
+auth.set_access_token('1305611268447928320-MA3D0D1Ay41wjefcRmrxKr08Ub12VM', 's0EAbYg9gKIM8qoq0XjDpCCA3mToGPvQxkntPv6AWbtVI')
 api = tp.API(auth, wait_on_rate_limit = True, wait_on_rate_limit_notify = True)
 try:
     api.verify_credentials()
